f386ba818140681f1228330a4530a84431d07c87
MohammedElKhadrawy <mohammedelkhadrawy641@gmail.com>
2026-08-28 21:54:34 +0300

    feat: implement AppBarChart component with Recharts integration and chart utilities

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import AppBarChart from "@/components/AppBarChart"
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>HomePage</h1>
+    <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 2xl:grid-cols-4">
+      <div className="rounded-lg bg-primary-foreground p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="rounded-lg bg-primary-foreground p-4">Test</div>
+      <div className="rounded-lg bg-primary-foreground p-4">Test</div>
+      <div className="rounded-lg bg-primary-foreground p-4">Test</div>
+      <div className="rounded-lg bg-primary-foreground p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        Test
+      </div>
+      <div className="rounded-lg bg-primary-foreground p-4">Test</div>
     </div>
   )
 }
