a359ee05da780aca3c63e38e813385a3e93dfcc4
MohammedElKhadrawy <mohammedelkhadrawy641@gmail.com>
2026-09-19 12:17:55 +0300

    feat: make Navbar sticky and add background color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   const { setTheme } = useTheme()
 
   return (
-    <nav className="flex items-center justify-between p-4">
+    <nav className="sticky top-0 z-10 flex items-center justify-between bg-background p-4">
       {/* LEFT */}
       <SidebarTrigger />
 
